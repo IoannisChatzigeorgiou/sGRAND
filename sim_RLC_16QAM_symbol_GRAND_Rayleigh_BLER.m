dcf65fd10d312077f3f7e1467aead3185c24ba17
Ioannis Chatzigeorgiou <101040331+IoannisChatzigeorgiou@users.noreply.github.com>
2023-04-20 10:50:57 +0100

    Updated publication information.

--- sim_RLC_16QAM_symbol_GRAND_Rayleigh_BLER.m
+++ sim_RLC_16QAM_symbol_GRAND_Rayleigh_BLER.m
@@ -7,7 +7,7 @@
 %
 % The system is described in 
 % "Symbol-Level GRAND for High-Order Modulation over Block Fading Channels",
-% which has been accepted for publication in IEEE Communications Letters.
+% IEEE Communications Letters, vol. 27, no. 2, Feb. 2023, pp. 447-451.
 % 
 % Authors: 
 % Ioannis Chatzigeorgiou, Lancaster University, United Kingdom
